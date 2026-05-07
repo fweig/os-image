436d14626e6ce014a1400e4d9dc0689f28843c84
Felix Weiglhofer <9267733+fweig@users.noreply.github.com>
2026-05-07 14:09:30 +0200

    Add gnome image viewer and thumbnails

--- build_files/quartz/install-gnome-apps.sh
+++ build_files/quartz/install-gnome-apps.sh
@@ -5,12 +5,15 @@ dnf5 --setopt=install_weak_deps=False install -y \
 	file-roller \
 	flatpak \
 	gdm \
+	glycin-loaders \
+	glycin-thumbnailer \
 	gnome-disk-utility \
 	gnome-shell \
 	gnome-software \
 	gnome-session \
 	gnome-session-wayland-session \
 	gnome-text-editor \
+	loupe \
 	nautilus \
 	ptyxis \
 	thunderbird # not a gnome app, but flatpak is too old
